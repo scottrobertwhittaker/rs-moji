2595abc567aa6ddf0502e8e2d2e6902754a21068
Scott Whittaker <scottrobertwhittaker@gmail.com>
2016-01-18 10:58:52 -0500

    Read code from specified file
    The example programs are from
    <https://github.com/steveklabnik/mojikun/tree/master/examples>.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![allow(dead_code)]
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 mod interpreter;
 mod lexer;
 mod parser;
 mod runtime;
 
 fn main() {
-    let code: &'static str =
-        "👍👍👍👍👍👍👍👍👍👍🔃👉👍👍👍👍👍👍👍👉👍👍👍👍👍👍👍👍👍👍👉👍👍👍👉👍👈👈👈👈👎🔙👉👍👍💻👉👍💻👍👍👍👍👍👍👍💻💻👍👍👍💻👉👍👍💻👈👈👍\
-         👍👍👍👍👍👍👍👍👍👍👍👍👍👍💻👉💻👍👍👍💻👎👎👎👎👎👎💻👎👎👎👎👎👎👎👎💻👉👍💻👉💻"; // = ARGF.read.chomp;
+    // TBD When are the right times to use `try!` vs. `unwrap`?
 
-    let tokens: Vec<char> = lexer::lex(code);
+    let filename = env::args().nth(1).unwrap();
+    let mut file = File::open(filename).unwrap();
+    let mut code = String::new();
+    file.read_to_string(&mut code).unwrap();
 
+    let tokens: Vec<char> = lexer::lex(&code);
     let ast: Vec<parser::Node> = parser::parse(tokens);
 
     let runtime = runtime::new();
-
     let mut interpreter = interpreter::new(runtime);
     interpreter.evaluate(ast);
 }
